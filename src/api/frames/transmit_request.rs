8211a7d54a6658bef31cb969ec70aad4a0040398
Kat <187942808+northernpaws@users.noreply.github.com>
2025-07-09 20:23:21 -0600

    test: add test for transmit_request frame

--- src/api/frames/transmit_request.rs
+++ src/api/frames/transmit_request.rs
@@ -93,14 +93,10 @@ impl Frame for TransmitRequestFrame {
     }
 
     /// Populates the provided packet buffer with the transmit request data.
-    fn populate_frame_data<'a> (&self, buffer: &mut PacketBuffer<'a>) -> Result<(), PacketSerializationError> {
+    fn encode_frame_data<'a> (&self, buffer: &mut PacketBuffer<'a>) -> Result<(), PacketSerializationError> {
         if self.payload.len() > 65535 - 112 {
             return Err(PacketSerializationError::PayloadTooLarge(self.payload.len()));
         }
-
-        // Encode the payload length.
-        // TODO: why +0x0e
-        buffer.put_u16((self.payload.len() as u16) + (0x0e as u16));
         
         // Set the command frame type.
         buffer.put_u8(FrameKind::TransmitRequest as u8);
@@ -116,7 +112,8 @@ impl Frame for TransmitRequestFrame {
         // Use 0x000000000000FFFF for broadcast, or use
         // 0x0000000000000000 to address the Zigbee coordinator.
         buffer.put_u64(self.dest_addr);
-        buffer.put_u16(0xfffe); // using 64-bit addresses, so set 16-bit field to 0xFFFE
+        // Using 64-bit addresses, so set 16-bit field to special value of 0xFFFE
+        buffer.put_u16(0xfffe);
 
         // Sets the maximum number of hops a broadcast transmission can traverse.
         //
@@ -136,3 +133,40 @@ impl Frame for TransmitRequestFrame {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_packet() -> Result<(), PacketSerializationError> {
+        let frame = TransmitRequestFrame{
+            frame_id: 1,
+            dest_addr: 0x0000000000000000,
+            broadcast_radius: 0,
+            options: None,
+            payload: b"HELLO",
+        };
+
+        // 65535 is the XBee max API packet size.
+        let mut buffer: [u8; 65535] = [0; 65535]; 
+        let mut packet_buffer = PacketBuffer::new(&mut buffer);
+        let size = frame.encode_frame(&mut packet_buffer)?;
+
+        assert_eq!(size, 23);
+        assert_eq!(packet_buffer.bytes(), [
+            0x7E, // delimiter
+            0x00, 0x13, // length
+            0x10, // type (transmit request)
+            0x01, // frame ID
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // destination address (coordinator)
+            0xFF, 0xFE, // 16-bit address (ignore)
+            0x00, // broadcast radius
+            0x00, // options
+            0x48, 0x45, 0x4C, 0x4C, 0x4F, // H E L L O
+            0x7D // checksum
+        ]);
+
+        Ok(())
+    }
+
+}
