6f3afd42f76de30810024527fdbbfeed48696b9e
Kat <187942808+northernpaws@users.noreply.github.com>
2025-07-10 14:15:30 -0600

    feat: start adding complete set of AT commands

--- src/at.rs
+++ src/at.rs
@@ -8,82 +8,96 @@ pub struct ATCommand<'a> {
 
 /// Enum that encapsulates all the well-known AT commands
 /// and translates them to AT command structs.
-pub enum ATCommands<'a> {
-    /// Causes the XBee radio to enter AT command mode from other states.
-    CommandMode(bool),
+/// 
+/// See: https://docs.digi.com/resources/documentation/digidocs/pdfs/90002273.pdf (p.g. 147)
+pub enum ATCommands {
+    // ==========
+    // Networking Commands
+
+    /// Sets the 802.15.4 channel to use for the network.
+    /// 
+    /// See: https://docs.digi.com/resources/documentation/digidocs/pdfs/90002273.pdf (p.g. 148)
+    OperatingChannel(u8),
+
+    /// Sets the Personal Area Network identifier for the device.
+    ExtendedPANID(u16),
+
+
+    // /// Causes the XBee radio to enter AT command mode from other states.
+    // CommandMode(bool),
     
-    /// Should cause the radio to reply with OK
-    Attention(),
+    // /// Should cause the radio to reply with OK
+    // Attention(),
     
-    /// Sets or retrieves the PAN ID for the radio.
-    PersonalAreaNetworkID(Option<&'a [u8]>),
+    // /// Sets or retrieves the PAN ID for the radio.
+    // PersonalAreaNetworkID(Option<&'a [u8]>),
 
-    /// Retrieves or set the upper and lower halfs of the XBee's address.
-    AddressUpper(Option<&'a [u8]>),
-    AddressLower(Option<&'a [u8]>),
+    // /// Retrieves or set the upper and lower halfs of the XBee's address.
+    // AddressUpper(Option<&'a [u8]>),
+    // AddressLower(Option<&'a [u8]>),
 
-    /// Retrieves or sets the address the XBee is communicating with.
-    DestinationAddressUpper(Option<&'a [u8]>),
-    DestinationAddressLower(Option<&'a [u8]>),
+    // /// Retrieves or sets the address the XBee is communicating with.
+    // DestinationAddressUpper(Option<&'a [u8]>),
+    // DestinationAddressLower(Option<&'a [u8]>),
 
-    /// Writes the current configuration to the device memory for next boot.
-    PersistConfiguration(),
-    /// Returns configuration to factory default.
-    ResetConfiguration(),
+    // /// Writes the current configuration to the device memory for next boot.
+    // PersistConfiguration(),
+    // /// Returns configuration to factory default.
+    // ResetConfiguration(),
 
-    /// Returns the XBee's 16 bit address.
-    MyAddress(),
+    // /// Returns the XBee's 16 bit address.
+    // MyAddress(),
 }
 
 impl ATCommands<'_> {
     pub fn create(&self) -> ATCommand {
         match *self {
-            ATCommands::CommandMode(ref state) => match state {
-                true => ATCommand {
-                    command: "+++",
-                    parameter: &None,
-                },
-                false => ATCommand {
-                    command: "CN",
-                    parameter: &None,
-                },
-            },
-            ATCommands::Attention() => ATCommand {
-                command: "AT",
-                parameter: &None,
-            },
-            ATCommands::PersonalAreaNetworkID(ref id) => ATCommand {
-                command: "ATID",
-                parameter: id,
-            },
-            ATCommands::AddressUpper(ref id) => ATCommand {
-                command: "ATSH",
-                parameter: id,
-            },
-            ATCommands::AddressLower(ref id) => ATCommand {
-                command: "ATSL",
-                parameter: id,
-            },
-            ATCommands::DestinationAddressUpper(ref id) => ATCommand {
-                command: "ATDH",
-                parameter: id,
-            },
-            ATCommands::DestinationAddressLower(ref id) => ATCommand {
-                command: "ATDL",
-                parameter: id,
-            },
-            ATCommands::PersistConfiguration() => ATCommand {
-                command: "ATWR",
-                parameter: &None,
-            },
-            ATCommands::ResetConfiguration() => ATCommand {
-                command: "ATRE",
-                parameter: &None,
-            },
-            ATCommands::MyAddress() => ATCommand {
-                command: "ATMY",
-                parameter: &None,
-            },
+            // ATCommands::CommandMode(ref state) => match state {
+            //     true => ATCommand {
+            //         command: "+++",
+            //         parameter: &None,
+            //     },
+            //     false => ATCommand {
+            //         command: "CN",
+            //         parameter: &None,
+            //     },
+            // },
+            // ATCommands::Attention() => ATCommand {
+            //     command: "AT",
+            //     parameter: &None,
+            // },
+            // ATCommands::PersonalAreaNetworkID(ref id) => ATCommand {
+            //     command: "ATID",
+            //     parameter: id,
+            // },
+            // ATCommands::AddressUpper(ref id) => ATCommand {
+            //     command: "ATSH",
+            //     parameter: id,
+            // },
+            // ATCommands::AddressLower(ref id) => ATCommand {
+            //     command: "ATSL",
+            //     parameter: id,
+            // },
+            // ATCommands::DestinationAddressUpper(ref id) => ATCommand {
+            //     command: "ATDH",
+            //     parameter: id,
+            // },
+            // ATCommands::DestinationAddressLower(ref id) => ATCommand {
+            //     command: "ATDL",
+            //     parameter: id,
+            // },
+            // ATCommands::PersistConfiguration() => ATCommand {
+            //     command: "ATWR",
+            //     parameter: &None,
+            // },
+            // ATCommands::ResetConfiguration() => ATCommand {
+            //     command: "ATRE",
+            //     parameter: &None,
+            // },
+            // ATCommands::MyAddress() => ATCommand {
+            //     command: "ATMY",
+            //     parameter: &None,
+            // },
             // TODO: ATD0…ATD7, ATP0…ATP1, ATIR, ATPR
         }
     }
